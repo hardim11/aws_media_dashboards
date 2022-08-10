ef0f05dd615ac3c7b23c8158f73f0c78dbe1de07
Matt Hardiman <matt.hardiman@outlook.com>
2022-08-10 23:33:48 +0100

    updated readme

--- README.md
+++ README.md
@@ -5,13 +5,21 @@ https://aws.amazon.com/blogs/media/cs-quick-and-easy-media-services-dashboards/
 
 # Implemented Classes
 * CloudwatchDashboard - just used to post the created JSON to AWS
+* JinjaRender - wrapper for the Jinja2 template renderer
 * EmxDashboard - Elemental Media Connect Dashboard builder
 * EmlDashboard - Elemental Media Live Dashboard builder
 * EmpDashboard - Elemental Media Package Dashboard builder
 
 I haven't done the other dashboards, yet, as I don't need them!
 
-# Change To Jinga
+# Change To Jinga2
 I've chnaged the code to use Jinja2 templates to make the code a little easier to read. Need to
 fully test but it should allow the templates to be quickly changed and edited.
+
+# TODO
+* make more generic
+* test!
+* build a command line interface
+* support better filtering by name and tags
+* support deleting "empty" dashboards
 
--- main.py
+++ main.py
@@ -8,8 +8,9 @@ from classes.emx_dashboard import EmxDashboard
 from classes.eml_dashboard import EmlDashboard
 from classes.emp_dashboard import EmpDashboard
 
-
+# this should match the name of the local AWS cli profile
 AWS_PROFILE="matt-root"
+# the region to scan for media services  and the dashboard is saved to
 AWS_REGION="eu-west-1"
 
 def logger(message):
@@ -18,17 +19,20 @@ def logger(message):
     """
     print(message)
 
-
+# object used to do the upload of the dashboard to AWS
 cw = CloudwatchDashboard(AWS_REGION, AWS_PROFILE)
 
+# create and upload the Elemental Media Connect Dashboard
 emx = EmxDashboard(AWS_REGION, AWS_PROFILE, logger)
 emx_widgets = emx.get_dashboards("")
 cw.publish_dashboard("MediaConnect_Flows-Python-Jinja", emx_widgets)
 
+# create and upload the Elemental Media Live Dashboard
 eml = EmlDashboard(AWS_REGION, AWS_PROFILE, logger)
 eml_widgets = eml.get_dashboards("")
 cw.publish_dashboard("MediaLive-Python-Jinja", eml_widgets)
 
+# create and upload the Elemental Media Package Dashboard
 emp = EmpDashboard(AWS_REGION, AWS_PROFILE, logger)
 emp_widgets = emp.get_dashboards("")
 cw.publish_dashboard("MediaPackage_Channels-Python-Jinja", emp_widgets)
